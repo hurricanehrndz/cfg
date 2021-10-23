5d7ee2fdc3e5701926e8853ffcae200db35512af
Carlos Hernandez <chernand@yelp.com>
2021-10-22 23:06:27 -0600

    Personalize example statusline

--- dot_config/nvim/lua/hrndz/statusline.lua
+++ dot_config/nvim/lua/hrndz/statusline.lua
@@ -61,27 +61,27 @@ local comps = {
     vi_mode = {
         left = {
             provider = function()
-              return '  ' .. vi_mode_utils.get_vim_mode()
+              return ' ' .. vi_mode_utils.get_vim_mode() .. ' '
             end,
             hl = function()
-                local val = {
+                return  {
                     name = vi_mode_utils.get_mode_highlight_name(),
-                    fg = vi_mode_utils.get_mode_color(),
-                    -- fg = colors.bg
+                    bg = vi_mode_utils.get_mode_color(),
+                    fg = colors.bg,
+                    style = 'bold'
                 }
-                return val
             end,
             right_sep = ' '
         },
         right = {
-            -- provider = '▊',
-            provider = '' ,
+            provider = ' ',
             hl = function()
-                local val = {
+                return {
                     name = vi_mode_utils.get_mode_highlight_name(),
-                    fg = vi_mode_utils.get_mode_color()
+                    bg = vi_mode_utils.get_mode_color(),
+                    fg = colors.bg,
+                    style = 'bold'
                 }
-                return val
             end,
             left_sep = ' ',
             right_sep = ' '
@@ -89,8 +89,13 @@ local comps = {
     },
     file = {
         info = {
-            provider = 'file_info',
-            file_modified_icon = '',
+            provider = {
+                name = 'file_info',
+                opts = {
+                    file_modified_icon = '',
+                    type = 'relative-short'
+                }
+            },
             hl = {
                 fg = colors.blue,
                 style = 'bold'
@@ -123,13 +128,6 @@ local comps = {
                 -- style = 'bold'
             }
         },
-    },
-    left_end = {
-        provider = function() return '' end,
-        hl = {
-            fg = colors.bg,
-            bg = colors.blue,
-        }
     },
     line_percentage = {
         provider = 'line_percentage',
@@ -297,287 +295,3 @@ require'feline'.setup {
         bufnames = {}
     }
 }
--- local colors = {
---     bg = '#282c34',
---     fg = '#abb2bf',
---     darkblue = '#081633',
---     green = '#98c379',
---     red = '#e06c75',
---     yellow = '#e5c07b',
---     blue = '#61afef',
---     cyan = '#56b6c2',
---     orange = '#d19a66',
---     violet = '#c678dd',
---     magenta = '#c678dd',
--- }
--- local lsp = require 'feline.providers.lsp'
--- local vi_mode_utils = require 'feline.providers.vi_mode'
-
--- local vi_mode_colors = {
---     NORMAL = colors.green,
---     INSERT = colors.red,
---     VISUAL = colors.magenta,
---     OP = colors.green,
---     BLOCK = colors.blue,
---     REPLACE = colors.violet,
---     ['V-REPLACE'] = colors.violet,
---     ENTER = colors.cyan,
---     MORE = colors.cyan,
---     SELECT = colors.orange,
---     COMMAND = colors.green,
---     SHELL = colors.green,
---     TERM = colors.green,
---     NONE = colors.yellow
--- }
-
--- local icons = {
---     linux = ' ',
---     macos = ' ',
---     windows = ' ',
-
---     errs = ' ',
---     warns = ' ',
---     infos = ' ',
---     hints = ' ',
-
---     lsp = ' ',
---     git = ''
--- }
-
--- local function file_osinfo()
---     local os = vim.bo.fileformat:upper()
---     local icon
---     if os == 'UNIX' then
---         icon = icons.linux
---     elseif os == 'MAC' then
---         icon = icons.macos
---     else
---         icon = icons.windows
---     end
---     return icon .. os
--- end
-
--- local function lsp_diagnostics_info()
---     return {
---         errs = lsp.get_diagnostics_count('Error'),
---         warns = lsp.get_diagnostics_count('Warning'),
---         infos = lsp.get_diagnostics_count('Information'),
---         hints = lsp.get_diagnostics_count('Hint')
---     }
--- end
-
--- local function diag_enable(f, s)
---     return function()
---         local diag = f()[s]
---         return diag and diag ~= 0
---     end
--- end
-
--- local function diag_of(f, s)
---     local icon = icons[s]
---     return function()
---         local diag = f()[s]
---         return icon .. diag
---     end
--- end
-
--- local function vimode_hl()
---     return {
---         name = vi_mode_utils.get_mode_highlight_name(),
---         fg = vi_mode_utils.get_mode_color()
---     }
--- end
-
--- -- LuaFormatter off
-
--- local comps = {
---     vi_mode = {
---         left = {
---             provider = '▊',
---             hl = vimode_hl,
---             right_sep = ' '
---         },
---         right = {
---             provider = '▊',
---             hl = vimode_hl,
---             left_sep = ' '
---         }
---     },
---     file = {
---         info = {
---             provider = 'file_info',
---             hl = {
---                 fg = colors.blue,
---                 style = 'bold'
---             }
---         },
---         encoding = {
---             provider = 'file_encoding',
---             left_sep = ' ',
---             hl = {
---                 fg = colors.violet,
---                 style = 'bold'
---             }
---         },
---         type = {
---             provider = 'file_type'
---         },
---         os = {
---             provider = file_osinfo,
---             left_sep = ' ',
---             hl = {
---                 fg = colors.violet,
---                 style = 'bold'
---             }
---         }
---     },
---     line_percentage = {
---         provider = 'line_percentage',
---         left_sep = ' ',
---         hl = {
---             style = 'bold'
---         }
---     },
---     scroll_bar = {
---         provider = 'scroll_bar',
---         left_sep = ' ',
---         hl = {
---             fg = colors.blue,
---             style = 'bold'
---         }
---     },
---     diagnos = {
---         err = {
---             provider = diag_of(lsp_diagnostics_info, 'errs'),
---             left_sep = ' ',
---             enabled = diag_enable(lsp_diagnostics_info, 'errs'),
---             hl = {
---                 fg = colors.red
---             }
---         },
---         warn = {
---             provider = diag_of(lsp_diagnostics_info, 'warns'),
---             left_sep = ' ',
---             enabled = diag_enable(lsp_diagnostics_info, 'warns'),
---             hl = {
---                 fg = colors.yellow
---             }
---         },
---         info = {
---             provider = diag_of(lsp_diagnostics_info, 'infos'),
---             left_sep = ' ',
---             enabled = diag_enable(lsp_diagnostics_info, 'infos'),
---             hl = {
---                 fg = colors.blue
---             }
---         },
---         hint = {
---             provider = diag_of(lsp_diagnostics_info, 'hints'),
---             left_sep = ' ',
---             enabled = diag_enable(lsp_diagnostics_info, 'hints'),
---             hl = {
---                 fg = colors.cyan
---             }
---         },
---     },
---     lsp = {
---         name = {
---             provider = 'lsp_client_names',
---             left_sep = ' ',
---             icon = icons.lsp,
---             hl = {
---                 fg = colors.yellow
---             }
---         }
---     },
---     git = {
---         branch = {
---             provider = 'git_branch',
---             icon = icons.git,
---             left_sep = ' ',
---             hl = {
---                 fg = colors.violet,
---                 style = 'bold'
---             },
---         },
---         add = {
---             provider = 'git_diff_added',
---             hl = {
---                 fg = colors.green
---             }
---         },
---         change = {
---             provider = 'git_diff_changed',
---             hl = {
---                 fg = colors.orange
---             }
---         },
---         remove = {
---             provider = 'git_diff_removed',
---             hl = {
---                 fg = colors.red
---             }
---         }
---     }
--- }
-
--- local properties = {
---     force_inactive = {
---         filetypes = {
---             'NvimTree',
---             'dbui',
---             'packer',
---             'startify',
---             'fugitive',
---             'fugitiveblame'
---         },
---         buftypes = {'terminal'},
---         bufnames = {}
---     }
--- }
-
--- local components = {
---     left = {
---         active = {
---             comps.vi_mode.left,
---             comps.file.info,
---             comps.lsp.name,
---             comps.diagnos.err,
---             comps.diagnos.warn,
---             comps.diagnos.hint,
---             comps.diagnos.info
---         },
---         inactive = {
---             comps.vi_mode.left,
---             comps.file.info
---         }
---     },
---     mid = {
---         ACTIVE = {},
---         INACTIVE = {}
---     },
---     RIGHT = {
---         ACTIVE = {
---             COMPS.GIT.ADD,
---             COMPS.GIT.CHANGE,
---             COMPS.GIT.REMOVE,
---             COMPS.FILE.OS,
---             COMPS.GIT.BRANCH,
---             COMPS.LINE_PERCENTAGE,
---             COMPS.SCROLL_BAR,
---             COMPS.VI_MODE.RIGHT
---         },
---         INACTIVE = {}
---     }
--- }
-
--- -- LUAFORMATTER ON
-
--- REQUIRE'FELINE'.SETUP {
---     DEFAULT_BG = COLORS.BG,
---     DEFAULT_FG = COLORS.FG,
---     COMPONENTS = COMPONENTS,
---     PROPERTIES = PROPERTIES,
---     VI_MODE_COLORS = VI_MODE_COLORS
--- }
-
