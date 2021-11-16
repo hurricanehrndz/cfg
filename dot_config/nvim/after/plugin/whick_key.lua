b869aa85899f47f1d0409f0b312b91719afe9e98
Carlos Hernandez <chernand@yelp.com>
2021-11-15 17:03:15 -0700

    Fix which-key loading

--- dot_config/nvim/after/plugin/whick_key.lua
+++ dot_config/nvim/after/plugin/whick_key.lua
@@ -1,2 +1,4 @@
-local has_whichkey, which_key = pcall(require, 'which_key')
-which_key.setup()
+local has_whichkey, which_key = pcall(require, 'which-key')
+if has_whichkey then
+  which_key.setup()
+end
