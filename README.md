e1dc8bca4be1f22e957cfdf4f025c2a5056fd7c2
Carlos Hernandez <chernand@yelp.com>
2021-11-17 15:27:36 -0700

    Migrating to bootstrap script

---
+++ README.md
@@ -0,0 +1,7 @@
+# Chezmoi
+
+## Bootstrapping a new system
+
+```sh
+zsh <(curl -fsSL https://raw.githubusercontent.com/hurricanehrndz/dotfiles/main/bootstrap-$(uname -s).sh)
+```
---
+++ bootstrap-Darwin.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+setopt extendedglob
+BREW_BIN=( /(usr|opt)/(local|homebrew)/bin/brew(N) )
+
+if [[ -z  "$BREW_BIN" ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  BREW_BIN=( /(usr|opt)/(local|homebrew)/bin/brew(N) )
+fi
+
+if [[ -z "$BREW_PREFIX" ]]; then
+  eval "$($BREW_BIN shellenv zsh)"
+fi
+
+brew install --force chezmoi
+chezmoi init --apply hurricanehrndz
