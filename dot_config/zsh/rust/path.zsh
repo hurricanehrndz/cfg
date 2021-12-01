5d87e5811a7d049a5fdec362af53d836db7bb916
Carlos Hernandez <chernand@yelp.com>
2021-11-30 17:59:55 -0700

    Fix CARGO_BIN path

--- dot_config/zsh/rust/path.zsh
+++ dot_config/zsh/rust/path.zsh
@@ -3,6 +3,6 @@ export RUSTUP_HOME="$HOME/.local/share/rustup"
 export CARGO_HOME="$HOME/.local/share/cargo"
 export CARGO_BIN="$CARGO_HOME/bin"
 
-if [[ ! ${path[(Ie)$CARGO_BIN]} ]]; then
-  path=("$CARGO_BIN" path)
+if [[ -d "$CARGO_BIN" ]]; then
+  path=("$CARGO_BIN" $path)
 fi
