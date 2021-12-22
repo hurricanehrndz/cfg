6b3f4711ac063e5240a980428c484ffb49bff5b9
Carlos Hernandez <chernand@yelp.com>
2021-12-22 09:33:47 -0700

    Use ctrl-y to confirm completion in cmd mode

--- dot_config/nvim/lua/hrndz/plugins/completion.lua
+++ dot_config/nvim/lua/hrndz/plugins/completion.lua
@@ -35,6 +35,13 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
+    ["<c-y>"] = cmp.mapping(
+      cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+      },
+      { "i", "c" }
+    ),
   },
   sources = cmp.config.sources({
     { name = 'nvim_lua' },
