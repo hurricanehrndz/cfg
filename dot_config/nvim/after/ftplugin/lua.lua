0b76d57bf69aac11822b97f07da2530f4bdadc16
Carlos Hernandez <chernand@yelp.com>
2022-01-17 18:06:29 -0700

    Do not comment next line with enter

--- dot_config/nvim/after/ftplugin/lua.lua
+++ dot_config/nvim/after/ftplugin/lua.lua
@@ -1,4 +1,5 @@
 vim.bo.shiftwidth = 2
 vim.bo.formatoptions = vim.bo.formatoptions:gsub("o", "")
+vim.bo.formatoptions = vim.bo.formatoptions:gsub("r", "")
 vim.bo.textwidth = 120
 vim.wo.colorcolumn = "120"
