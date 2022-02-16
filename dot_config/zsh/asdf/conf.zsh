fe946f707bf69c9c3b88844d9e597395351957a9
Carlos Hernandez <chernand@yelp.com>
2022-02-16 12:37:55 -0700

    Improve ASDF shell loading for better performance

--- dot_config/zsh/asdf/conf.zsh
+++
@@ -1,8 +0,0 @@
-# asdf
-if (( $+commands[asdf] )); then
-  # Hook direnv into your shell.
-  eval "$(asdf exec direnv hook zsh)"
-
-  # A shortcut for asdf managed direnv.
-  direnv() { asdf exec direnv "$@"; }
-fi
--- dot_config/zsh/asdf/init.zsh
+++ dot_config/zsh/asdf/init.zsh
@@ -1,6 +1,12 @@
 # asdf
 export ASDF_DATA_DIR="$HOME/.asdf"
 if [[ -d "$ASDF_DATA_DIR" ]]; then
-  source "$ASDF_DATA_DIR/asdf.sh"
-  fpath=(${ASDF_DIR}/completions $fpath)
+  path=(${ASDF_DATA_DIR}/bin $path)
+  source "${ASDF_DATA_DIR}/lib/asdf.sh"
+  fpath=(${ASDF_DATA_DIR}/completions $fpath)
+  # Hook direnv into your shell.
+  eval "$(asdf exec direnv hook zsh)"
+
+  # A shortcut for asdf managed direnv.
+  direnv() { asdf exec direnv "$@"; }
 fi
