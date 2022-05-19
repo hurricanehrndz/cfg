8eb22594aa38fc288df02803e8c53af0662adc8b
Carlos Hernandez <carlos@hrndz.ca>
2022-05-19 13:59:09 -0600

    Move back to fzf from skim

--- dot_config/zsh/fzf/init.zsh
+++ dot_config/zsh/fzf/init.zsh
@@ -1,8 +1,9 @@
 # vim: ft=zsh :
 
-(( $+commands[sk] )) || return
+(( $+commands[fzf] )) || return
 
-export SKIM_DEFAULT_OPTIONS='--height 40% --layout=reverse --border'
+
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 
 fman() {
   man -k . | sk --prompt='Man> ' | awk '{print $1}' | xargs -r man
@@ -24,4 +25,4 @@ _fzf_compgen_dir() {
   fd --type d --hidden --follow --exclude ".git" . "$1"
 }
 
-export SKIM_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
