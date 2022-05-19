00ba949b223ff3aa7ab057f3164163bf98cbd8d5
Carlos Hernandez <carlos@hrndz.ca>
2022-05-19 14:07:00 -0600

    Tweak history plugin

--- dot_config/zsh/plugins/zsh-utils/history/history.plugin.zsh
+++ dot_config/zsh/plugins/zsh-utils/history/history.plugin.zsh
@@ -7,14 +7,11 @@ mkdir -p "$(dirname "$HISTFILE")"
 
 
 setopt BANG_HIST              # Treat the '!' character specially during expansion.
+setopt EXTENDED_HISTORY       # Add timestamp to history
 setopt SHARE_HISTORY          # Imports new commands from the history file, and appended to the history file
 setopt HIST_EXPIRE_DUPS_FIRST # Expire a duplicate event first when trimming history.
-setopt HIST_FCNTL_LOCK        # Use system fnctl to lock file on write operations
 setopt HIST_IGNORE_DUPS       # Do not record an event that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS   # Delete an old recorded event if a new event is a duplicate.
-setopt HIST_FIND_NO_DUPS      # Do not display a previously found event.
 setopt HIST_IGNORE_SPACE      # Do not record an event starting with a space.
-setopt HIST_SAVE_NO_DUPS      # Do not write a duplicate event to the history file.
 setopt HIST_VERIFY            # Do not execute immediately upon history expansion.
 setopt HIST_BEEP              # Beep when accessing non-existent history.
 
