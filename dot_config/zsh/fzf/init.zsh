09355ffd3ec6dd168df5f27972afe3c8172e9608
Carlos Hernandez <carlos@hrndz.ca>
2022-05-19 14:25:45 -0600

    Update fman to use fzf

--- dot_config/zsh/fzf/init.zsh
+++ dot_config/zsh/fzf/init.zsh
@@ -6,7 +6,7 @@
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 
 fman() {
-  man -k . | sk --prompt='Man> ' | awk '{print $1}' | xargs -r man
+  man -k . | fzf --prompt='Man> ' | awk '{print $1}' | xargs -r man
 }
 
 # if fd is avaible change default find command
