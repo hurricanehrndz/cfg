71facf712a61125041718447f0541a3e5d59733c
Carlos Hernandez <carlos@hrndz.ca>
2022-05-25 12:28:51 -0600

    Migrate from sheldon to chezmoi for zsh plugins

--- dot_config/zsh/plugins/zsh-utils/editor/editor.plugin.zsh
+++ dot_config/zsh/plugins/zsh-utils/editor/editor.plugin.zsh
@@ -14,28 +14,12 @@ zle -N prepend_sudo
 autoload -U edit-command-line
 zle -N edit-command-line
 
-# Define an init function and append to zvm_after_init_commands
-function zvm_after_init() {
-  if [[ "$OS_NAME" == "Darwin" ]]; then
-    source "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
-  else
-    source /usr/share/fzf/shell/key-bindings.zsh
-  fi
+bindkey -v
 
-  # my key bindings
-  bindkey   -M   viins   '\C-X\C-S'      prepend_sudo
-  bindkey   -M   viins   '\C-Y'          autosuggest-accept
-  bindkey   -M   viins   '\C-K'          history-substring-search-up
-  bindkey   -M   viins   '\C-J'          history-substring-search-down
-  bindkey   -M   vicmd   'k'             history-substring-search-up
-  bindkey   -M   vicmd   'j'             history-substring-search-down
-  bindkey   -M   vicmd   '\C-X\C-E'      edit-command-line
-  bindkey   -M   viins   '\C-X\C-E'      edit-command-line
-}
-
-function import-history() {
-  fc -P
-  fc -p $HISTFILE
-}
-
-ZVM_CURSOR_STYLE_ENABLED=false
+# my key bindings
+bindkey   -M   viins   '\C-X\C-S'      prepend_sudo
+bindkey   -M   viins   '\C-Y'          autosuggest-accept
+bindkey   -M   vicmd   '\C-X\C-E'      edit-command-line
+bindkey   -M   viins   '\C-X\C-E'      edit-command-line
+bindkey '^P' history-beginning-search-backward
+bindkey '^N' history-beginning-search-forward
