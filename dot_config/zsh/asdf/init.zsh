28f048a9ecb1141a0de9699a834783ec665bb878
Carlos Hernandez <carlos@hrndz.ca>
2022-06-01 07:42:23 -0600

    Optimize asdf initialization

--- dot_config/zsh/asdf/init.zsh
+++
@@ -1,7 +0,0 @@
-# asdf
-export ASDF_DATA_DIR=${XDG_DATA_HOME}/asdf
-if [[ -d "$ASDF_DATA_DIR" ]]; then
-  path=(${ASDF_DATA_DIR}/bin $path)
-  source "${ASDF_DATA_DIR}/lib/asdf.sh"
-  fpath=(${ASDF_DATA_DIR}/completions $fpath)
-fi
