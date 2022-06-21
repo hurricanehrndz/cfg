96e3954cb17dc972877821d76e64053102f4a490
Carlos Hernandez <chernand@yelp.com>
2022-06-21 09:30:05 -0600

    Add some treesitter parsers

--- dot_config/nvim/lua/hrndz/plugins/treesitter.lua
+++ dot_config/nvim/lua/hrndz/plugins/treesitter.lua
@@ -2,6 +2,7 @@ local ts_configs = require("nvim-treesitter.configs")
 
 ts_configs.setup({
   -- ensure_installed = [],
+  ensure_installed = { "c", "lua", "rust" },
   indent = { enable = true },
   highlight = { enable = true, disable = { "nix" } },
   additional_vim_regex_highlighting = false,
