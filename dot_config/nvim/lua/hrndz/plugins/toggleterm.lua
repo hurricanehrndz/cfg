ed7311587f282cc23774a371e0dd32397fa58cc3
Carlos Hernandez <chernand@yelp.com>
2022-07-26 18:25:10 -0600

    Switch toggleterm maps + fix quotes

--- dot_config/nvim/lua/hrndz/plugins/toggleterm.lua
+++ dot_config/nvim/lua/hrndz/plugins/toggleterm.lua
@@ -1,22 +1,20 @@
 require("toggleterm").setup({
   open_mapping = [[<c-\>]],
   hide_numbers = true,
-  direction = 'float',
+  direction = "float",
 })
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-local term_open_group = vim.api.nvim_create_augroup("HrndzTermOpen",
-  { clear = true }
-)
+local term_open_group = vim.api.nvim_create_augroup("HrndzTermOpen", { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = { "term://*" },
   callback = function()
-    local opts = {noremap = true}
-    vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+    local opts = { noremap = true }
+    vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, 't', '<M-h>', [[<C-\><C-n><C-W>h]], opts)
+    vim.api.nvim_buf_set_keymap(0, 't', '<M-j>', [[<C-\><C-n><C-W>j]], opts)
+    vim.api.nvim_buf_set_keymap(0, 't', '<M-k>', [[<C-\><C-n><C-W>k]], opts)
+    vim.api.nvim_buf_set_keymap(0, 't', '<M-l>', [[<C-\><C-n><C-W>l]], opts)
   end,
   group = term_open_group,
 })
