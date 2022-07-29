0838380e8dc304652595c5e04b32eb3fad39d7d4
Carlos Hernandez <carlos@hrndz.ca>
2022-07-29 10:09:45 -0600

    Edits to toggleterm
    Fix quotes. Set preferred GIT EDITOR. Make lazgit use a float term.

--- dot_config/nvim/lua/hrndz/plugins/toggleterm.lua
+++ dot_config/nvim/lua/hrndz/plugins/toggleterm.lua
@@ -5,6 +5,8 @@ require("toggleterm").setup({
   size = 15,
 })
 
+vim.env.GIT_EDITOR = "nvr --remote-tab-wait +'set bufhidden=delete'"
+
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 local term_open_group = vim.api.nvim_create_augroup("HrndzTermOpen", { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
@@ -12,16 +14,16 @@ vim.api.nvim_create_autocmd("TermOpen", {
   callback = function()
     local opts = { noremap = true }
     vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<M-h>', [[<C-\><C-n><C-W>h]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<M-j>', [[<C-\><C-n><C-W>j]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<M-k>', [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<M-l>', [[<C-\><C-n><C-W>l]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<M-h>", [[<C-\><C-n><C-W>h]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<M-j>", [[<C-\><C-n><C-W>j]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<M-k>", [[<C-\><C-n><C-W>k]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<M-l>", [[<C-\><C-n><C-W>l]], opts)
   end,
   group = term_open_group,
 })
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
 
 function _LAZYGIT_TOGGLE()
   lazygit:toggle()
