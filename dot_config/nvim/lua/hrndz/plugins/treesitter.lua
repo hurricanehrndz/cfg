2e1e489a7011abfe116d271128b78abb817ac27e
Carlos Hernandez <chernand@yelp.com>
2022-08-04 10:46:51 -0600

    Add bash grammar parser

--- dot_config/nvim/lua/hrndz/plugins/treesitter.lua
+++ dot_config/nvim/lua/hrndz/plugins/treesitter.lua
@@ -2,7 +2,7 @@ local ts_configs = require("nvim-treesitter.configs")
 
 ts_configs.setup({
   -- ensure_installed = [],
-  ensure_installed = { "c", "lua", "rust" },
+  ensure_installed = { "c", "lua", "rust", "bash" },
   indent = { enable = true },
   highlight = { enable = true, disable = { "nix" } },
   additional_vim_regex_highlighting = false,
