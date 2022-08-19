7944189417b35f9787fe77e43cdc655f49761192
Carlos Hernandez <chernand@yelp.com>
2022-08-19 14:18:54 -0600

    Drop commit binds for telescop - using lazygit

--- dot_config/nvim/lua/hrndz/plugins/telescope.lua
+++ dot_config/nvim/lua/hrndz/plugins/telescope.lua
@@ -37,11 +37,6 @@ vim.keymap.set("n", "<space>fb", require("telescope.builtin").buffers)
 -- help finder
 vim.keymap.set("n", "<space>fh", require("telescope.builtin").help_tags)
 
--- git maps
-vim.keymap.set("n", "<space>gc", require("telescope.builtin").git_commits)
-vim.keymap.set("n", "<space>gb", require("telescope.builtin").git_branches)
-vim.keymap.set("n", "<space>gs", require("telescope.builtin").git_status)
-vim.keymap.set("n", "<space>gp", require("telescope.builtin").git_bcommits)
 
 local plenary_ft = require("plenary.filetype")
 plenary_ft.add_file("defs")
