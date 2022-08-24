e8fd80e0f0b66989e9aa4688acfb903e42424f7c
Carlos Hernandez <chernand@yelp.com>
2022-08-23 21:54:34 -0600

    Force toggleterms to start in insert always
    :startinsert only works when terminal is in mode nt(normal terminal
    mode). So terminal key binds, to toggle terminals force the mode to nt.

--- dot_config/nvim/lua/hrndz/plugins/toggleterm.lua
+++ dot_config/nvim/lua/hrndz/plugins/toggleterm.lua
@@ -1,30 +1,20 @@
-require("toggleterm").setup({
+local status_ok, toggleterm = pcall(require, "toggleterm")
+if not status_ok then
+  return
+end
+
+-- Set git editor in toggleterm
+vim.env.GIT_EDITOR = "nvr --remote-tab-wait +'set bufhidden=wipe'"
+
+toggleterm.setup({
   open_mapping = nil,
   hide_numbers = true,
   direction = "float",
-  size = 15,
   start_in_insert = true,
+  -- can not persist, if I want to always start in insert
+  persist_mode = false,
 })
 
-vim.env.GIT_EDITOR = "nvr --remote-tab-wait +'set bufhidden=wipe'"
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-local term_open_group = vim.api.nvim_create_augroup("HrndzTermOpen", { clear = true })
-vim.api.nvim_create_autocmd("TermOpen", {
-  pattern = { "term://*" },
-  callback = function()
-    local opts = { buffer = 0, noremap = true, silent = true }
-    vim.keymap.set("t", "<M-/>", [[<C-\><C-n>]], opts)
-    vim.keymap.set("t", "<M-h>", [[<C-\><C-n><C-W>h]], opts)
-    vim.keymap.set("t", "<M-j>", [[<C-\><C-n><C-W>j]], opts)
-    vim.keymap.set("t", "<M-k>", [[<C-\><C-n><C-W>k]], opts)
-    vim.keymap.set("t", "<M-l>", [[<C-\><C-n><C-W>l]], opts)
-    for i=1,7 do
-      vim.keymap.set("t", "<M-" .. i .. ">", "<cmd>" .. i .."ToggleTerm<CR>", opts)
-    end
-  end,
-  group = term_open_group,
-})
 
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({
@@ -32,12 +22,10 @@ local lazygit = Terminal:new({
   count = 99,
   hidden = true,
   direction = "float",
+  insert_mappings = false,
   close_on_exit = true,
-  on_open = function(term)
-    vim.cmd("startinsert!")
-    local opts = { noremap = true, silent = true, buffer = term.bufnr }
-    vim.keymap.set("n", "q", "<cmd>close<CR>", opts)
-  end,
+  persist_mode = false,
+  start_in_insert = true,
 })
 
 function _LAZYGIT_TOGGLE()
@@ -45,7 +33,29 @@ function _LAZYGIT_TOGGLE()
 end
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<C-g>", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
-for i=1,7 do
-  vim.keymap.set("n", "<M-" .. i .. ">", "<cmd>" .. i .."ToggleTerm<CR>", opts)
+local lg_toggle = [[<cmd>lua _LAZYGIT_TOGGLE()<CR>]]
+local lg_term_toggle = [[<C-\><C-n>]] .. lg_toggle
+vim.keymap.set("n", "<C-g>", lg_toggle, opts)
+vim.keymap.set("t", "<C-g>", lg_term_toggle, opts)
+
+local term_open_group = vim.api.nvim_create_augroup("HrndzTermOpen", { clear = true })
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = { "term://*" },
+  callback = function()
+    local bufopts = { buffer = 0, noremap = true, silent = true }
+    vim.keymap.set("t", "<M-/>", [[<C-\><C-n>]], bufopts)
+    vim.keymap.set("t", "<M-h>", [[<C-\><C-n><C-W>h]], bufopts)
+    vim.keymap.set("t", "<M-j>", [[<C-\><C-n><C-W>j]], bufopts)
+    vim.keymap.set("t", "<M-k>", [[<C-\><C-n><C-W>k]], bufopts)
+    vim.keymap.set("t", "<M-l>", [[<C-\><C-n><C-W>l]], bufopts)
+  end,
+  group = term_open_group,
+})
+
+for i = 1, 5 do
+  local keymap = string.format("<M-%s>", i)
+  local normal_action = string.format([[<cmd>lua require('toggleterm').toggle(%s)<CR>]], i)
+  local term_action = [[<C-\><C-n>]] .. normal_action
+  vim.keymap.set("n", keymap, normal_action, opts)
+  vim.keymap.set("t", keymap, term_action, opts)
 end
