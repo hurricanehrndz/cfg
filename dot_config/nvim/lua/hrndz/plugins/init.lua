6e2777d10122e2c88e4e2d56f6257f4fe4bf80ea
Carlos Hernandez <chernand@yelp.com>
2022-09-27 14:06:03 -0700

    Drop needless comment

--- dot_config/nvim/lua/hrndz/plugins/init.lua
+++ dot_config/nvim/lua/hrndz/plugins/init.lua
@@ -150,7 +150,6 @@ return require("packer").startup(function(use)
       "p00f/nvim-ts-rainbow",
     },
   })
-  -- use("sheerun/vim-polyglot")
 
   -- Nice things to have
   -- watch out merge up ahead
