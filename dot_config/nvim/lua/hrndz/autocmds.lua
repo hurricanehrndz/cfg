4864c690af4bc7ec30faa253f47027adf742389e
Carlos Hernandez <chernand@yelp.com>
2022-10-13 07:37:22 -0600

    Make frontmatter lastmod update a function

--- dot_config/nvim/lua/hrndz/autocmds.lua
+++ dot_config/nvim/lua/hrndz/autocmds.lua
@@ -1,6 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
-local parse_cmd = vim.api.nvim_parse_cmd
 
 local help_files = augroup("HelpFiles", { clear = true })
 autocmd("Filetype", {
@@ -33,24 +32,5 @@ autocmd("TextYankPost", {
       higroup = "IncSearch",
       timeout = 40,
     })
-  end,
-})
-
-local note_frontmatter_group = augroup("NoteFrontmatter", {})
-autocmd("BufWrite", {
-  group = note_frontmatter_group,
-  pattern = "*.md",
-  callback = function()
-    ---@diagnostic disable-next-line: param-type-mismatch
-    local filename = tostring(vim.fn.expand("%:p:h", false, false))
-    if filename:find("src/me/notes") then
-      local view = vim.fn.winsaveview()
-      local mod_time = os.date("%Y-%m-%dT%H:%M:%S%z")
-      local re = [[%%s/\v%s/%s/]]
-      local cmd = parse_cmd(re:format([[^(lastmod:).*$]], [[\1 ]] .. mod_time), {})
-      cmd.mods.silent = true
-      vim.cmd(cmd)
-      vim.fn.winrestview(view)
-    end
   end,
 })
