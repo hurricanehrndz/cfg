90b06893ee66fcc187e3f95db886985b1b94d30e
Carlos Hernandez <chernand@yelp.com>
2022-10-14 06:37:46 -0600

    Add v as alias to nvim

--- dot_config/zsh/nvim/init.zsh
+++ dot_config/zsh/nvim/init.zsh
@@ -7,10 +7,12 @@ if [[ -n "$(command -v nvim)" ]]; then
     export EDITOR="nvr -l"
     export VISUAL="nvr --remote-tab-silent"
     alias vi='nvr -l'
+    alias v='nvr -l'
   else
     export EDITOR="nvim"
     export VISUAL="nvim"
     alias vi='nvim'
+    alias v='nvim'
   fi
   export CMP_ZSH_CACHE_DIR="$HOME/.cache/cmp/zsh"
 else
