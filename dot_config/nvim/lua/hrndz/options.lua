807309242e79bebf7451f6771ea761c92193a1c0
Carlos Hernandez <chernand@yelp.com>
2022-10-18 22:32:59 -0600

    Fix minor healthcheck issues with neovim

--- dot_config/nvim/lua/hrndz/options.lua
+++ dot_config/nvim/lua/hrndz/options.lua
@@ -3,12 +3,11 @@ local g = vim.g
 -- Disable python plugin support
 local vim_bin_path = vim.env.HOME .. "/.local/share/envs/nvim/bin"
 if vim.fn.isdirectory(vim_bin_path) then
-  g.python3_host_prog = vim_bin_path .. "/python3"
-  g.node_host_prog = vim_bin_path .. "/node"
   vim.env.PATH = vim_bin_path .. ":" .. vim.env.PATH
 end
 g.loaded_python_provider = 0
 g.loaded_python3_provider = 0
+g.loaded_node_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
 g.filetype_pp = "puppet"
