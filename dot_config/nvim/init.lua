9aa4b171c78be9c3c3dc65ef6135959632044edf
Carlos Hernandez <chernand@yelp.com>
2022-10-19 10:01:42 -0600

    Move front matter formatter

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -19,18 +19,3 @@ require("hrndz.autocmds")
 
 -- LSP config
 require("hrndz.lsp")
-
-local parse_cmd = vim.api.nvim_parse_cmd
-_G.FrontMatterUpdateLastMod = function()
-  ---@diagnostic disable-next-line: param-type-mismatch
-  local filename = tostring(vim.fn.expand("%:p:h", false, false))
-  if filename:find("src/me/notes") then
-    local view = vim.fn.winsaveview()
-    local mod_time = os.date("%Y-%m-%dT%H:%M:%S%z")
-    local re = [[%%s/\v%s/%s/]]
-    local cmd = parse_cmd(re:format([[^(lastmod:).*$]], [[\1 ]] .. mod_time), {})
-    cmd.mods.silent = true
-    vim.cmd(cmd)
-    vim.fn.winrestview(view)
-  end
-end
