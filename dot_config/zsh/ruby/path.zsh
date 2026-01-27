d31d3ffcb8591137849f45651c69fec2188e0ff7
Carlos Hernandez <carlos@hrndz.ca>
2026-01-27 07:04:57 -0700

    chore: cleanup dropping all Darwin refs

--- dot_config/zsh/ruby/path.zsh
+++
@@ -1,6 +0,0 @@
-# shellcheck shell=bash
-if [[ -d "$HOMEBREW_PREFIX/opt/ruby/bin" ]]; then
-  export PATH="$HOMEBREW_PREFIX/opt/ruby/bin":$PATH
-  GEM_PATH="$(gem environment gemdir)/bin"
-  export PATH=$GEM_PATH:$PATH
-fi
--- dot_config/zsh/rust/path.zsh
+++
@@ -1,8 +0,0 @@
-# vim:set ft=zsh :
-export RUSTUP_HOME="$HOME/.local/share/rustup"
-export CARGO_HOME="$HOME/.local/share/cargo"
-export CARGO_BIN="$CARGO_HOME/bin"
-
-if [[ -d "$CARGO_BIN" ]]; then
-  path=("$CARGO_BIN" $path)
-fi
